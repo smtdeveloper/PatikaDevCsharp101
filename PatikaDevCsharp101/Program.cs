49cb4da42232dd489051553313b4ca588426fb15
Samet Akca <developer2@cevizsoft.com>
2022-04-20 14:47:01 +0300

    Karar yapıları - if else

--- PatikaDevCsharp101/Program.cs
+++ PatikaDevCsharp101/Program.cs
@@ -229,6 +229,26 @@ namespace PatikaDevCsharp101
             Console.WriteLine("rakam1: " + rakam1);
             Console.WriteLine("double1: " + double1);
 
+
+
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 11)
+            {
+                Console.WriteLine("Günaydın !");
+            }
+            else if (time <= 18)
+            {
+                Console.WriteLine("İyi Günler !");
+            }
+            else
+            {
+                Console.WriteLine("İyi Geceler !");
+            }
+
+            string sonuc = time <= 18 ? "İyi Günler !" : "İyi Geceler !";
+            sonuc = time >= 6 && time < 11 ? "Günaydın !" : time <= 18 ? "İyi Günler !" : "İyi Geceler !";
+            Console.WriteLine(sonuc);
         }
     }
 }
