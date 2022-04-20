fcbc111da6b66e338e1cc59d4c613273d3a90a5c
Samet Akca <developer2@cevizsoft.com>
2022-04-20 17:54:32 +0300

    projeler acıldı

---
+++ Metot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Metot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
