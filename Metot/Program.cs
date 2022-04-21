ef832bce8665ec2b16e756b36f6c1b98d76ccc04
Samet Akca <developer2@cevizsoft.com>
2022-04-21 23:55:23 +0300

    Metot Overloading ve Out

--- Metot/Program.cs
+++ Metot/Program.cs
@@ -26,11 +26,41 @@ namespace Metot
             System.Console.WriteLine(Convert.ToString(sonuc2));
             System.Console.WriteLine(Convert.ToString(a + b));
 
+            string sayi = "999";
+
+            bool sonuc3 = int.TryParse(sayi, out int outSayi);
+            if (sonuc3)
+            {
+                Console.WriteLine("Başarılı!");
+                Console.WriteLine(outSayi);
+            }
+            else
+            {
+                Console.WriteLine("Başarısız!");
+            }
+
+            Metotlar2 instance = new Metotlar2();
+            instance.Topla2(4, 5, out int toplamSonucu);
+            Console.WriteLine(toplamSonucu);
+
+            //Metot Aşırı Yükleme - Overloading
+            int ifade = 999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("Fatih", "Hacıoğlu");
+
+            //Metot İmzası
+            //metot adı + parametre sayısı + parametre
+
+
+
         }
         static int Topla(int deger1, int deger2)
         {
             return (deger1 + deger2);
         }
+
+
 
     }
 
@@ -46,6 +76,31 @@ namespace Metot
             deger1 += 1;
             deger2 += 1;
             return deger1 + deger2;
+        }
+
+       
+
+    }
+
+    class Metotlar2
+    {
+        public void Topla2(int a, int b, out int toplam)
+        {
+            toplam = a + b;
+        }
+
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(string veri1, string veri2)
+        {
+            Console.WriteLine(veri1 + veri2);
         }
     }
 }
