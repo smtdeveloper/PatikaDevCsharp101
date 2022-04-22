c39fd23f46e1a4e59701cba9af4f830f290fee9c
Samet Akca <developer2@cevizsoft.com>
2022-04-23 01:20:12 +0300

    Dictionary<int, string>

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -4,13 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Dictionary
+namespace dictionary
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test push");
+
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+
+            dic.Add(5, "Eslem");
+            dic.Add(3, "Samet");
+            dic.Add(99, "sa");
+
+            // Dizilerde erişim
+            Console.WriteLine(dic[5]);
+
+            Console.WriteLine(" ");
+
+            foreach (var item in dic)
+                Console.WriteLine(item);
+
+            //Count
+            
+            Console.WriteLine("Adet : " + dic.Count);
+
+            //ContainsKey & ContainsValue
+            Console.WriteLine(dic.ContainsKey(3));
+            Console.WriteLine(dic.ContainsValue("sa"));
+
+            //Remove
+
+            dic.Remove(99);
+
+            // key & value
+
+            foreach (var item in dic)
+                Console.WriteLine(item.Key);
+
+            foreach (var item in dic)
+                Console.WriteLine(item.Value);
         }
     }
 }
