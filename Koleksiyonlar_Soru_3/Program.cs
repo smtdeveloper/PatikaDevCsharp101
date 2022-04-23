2baae7e9d1d5370e6b118384ef717b28de89ca80
Samet Akca <developer2@cevizsoft.com>
2022-04-23 16:04:18 +0300

    Odev2- Koleksiyonlar_Soru_3

---
+++ Koleksiyonlar_Soru_3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koleksiyonlar_Soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Soru - 3: Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız.
+
+            Console.WriteLine("Bir cümle giriniz : ");
+            string cumle = Console.ReadLine();
+            ArrayList sesliHarfler = new ArrayList();
+            char[] sabitSesliHarfler = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü' };
+            char[] harfler = new char[cumle.Length];
+            harfler = cumle.ToCharArray();
+            foreach (var harf in harfler)
+            {
+                foreach (var sabitSesli in sabitSesliHarfler)
+                {
+                    if (harf == sabitSesli)
+                    {
+                        sesliHarfler.Add(harf);
+                    }
+                }
+
+            }
+
+            Console.WriteLine("Cümledeki Sesli Harfler ; ");
+            foreach (var item in sesliHarfler)
+            {
+                Console.Write(  item + ", ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
