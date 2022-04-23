44a742fbe13e4919136514527264a59215b1f7a6
Samet Akca <developer2@cevizsoft.com>
2022-04-24 01:09:31 +0300

    Encapsulation Kavramı

---
+++ EncapsulationKavramı/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationKavramı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci = new Ogrenci();
+            ogrenci.Isim = "Samet";
+            ogrenci.Soyisim = "Akca";
+            ogrenci.OgrenciNo = 365;
+            ogrenci.Sinif = 3;
+            ogrenci.OgrenciBilgileriniGetir();
+
+            ogrenci.SinifAtlat();
+            ogrenci.OgrenciBilgileriniGetir();
+
+            Ogrenci ogrenci1 = new Ogrenci("Leyla   ", "gumus", 999, 1);
+            ogrenci1.OgrenciBilgileriniGetir();
+
+            ogrenci1.SinifDusur();
+            ogrenci1.OgrenciBilgileriniGetir();
+        }
+    }
+    class Ogrenci
+    {
+        private string isim; //field
+
+        private string soyisim;
+        private int ogrenciNo;
+        private int sinif;
+
+        public string Isim
+        {
+            get { return isim; }
+            set { isim = value; }
+        }
+        public string Soyisim
+        {
+            get => soyisim;
+            set => soyisim = value;
+        }
+        public int OgrenciNo { get => ogrenciNo; set => ogrenciNo = value; }
+        public int Sinif
+        {
+            get => sinif;
+            set
+            {
+                if (value >= 1)
+                {
+                    Console.WriteLine("Sınıf en az 1 olabilir.");
+                    sinif = 1;
+                }
+                else
+                {
+                    sinif = value;
+                }
+            }
+        }
+
+        public Ogrenci(string ısim, string soyisim, int ogrenciNo, int sinif)
+        {
+            Isim = ısim;
+            Soyisim = soyisim;
+            OgrenciNo = ogrenciNo;
+            Sinif = sinif;
+        }
+
+        public Ogrenci()
+        {
+
+        }
+
+        public void OgrenciBilgileriniGetir()
+        {
+            Console.WriteLine("***** Öğrenci Bilgileri *****");
+            Console.WriteLine("Öğrenci Adı : {0}", this.Isim);
+            Console.WriteLine("Öğrenci Soyadı : {0}", this.Soyisim);
+            Console.WriteLine("Öğrenci Numarası : {0}", this.OgrenciNo);
+            Console.WriteLine("Öğrenci Sınıfı : {0}", this.Sinif);
+
+        }
+        public void SinifAtlat()
+        {
+            this.Sinif = this.Sinif + 1;
+        }
+        public void SinifDusur()
+        {
+            this.Sinif = this.Sinif - 1;
+        }
+    }
+    }
+}
