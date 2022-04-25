805baceeafa892b79439dfe4a5cc6dea700112b2
Samet Akca <developer2@cevizsoft.com>
2022-04-25 03:52:23 +0300

    ToDo Uygulaması

---
+++ ToDoUygulaması/Kart.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoUygulaması
+{
+    class Kart
+    {
+        public string Baslik { get; set; }
+        public string Icerik { get; set; }
+        public string AtananKisi { get; set; }
+        public Buyuluk Boyut { get; set; }
+
+        public enum Buyuluk
+        {
+            XS,
+            S,
+            M,
+            L,
+            XL
+        }
+
+        public Kart(string baslik, string icerik, string atananKisi, Buyuluk boyut)
+        {
+            Baslik = baslik;
+            Icerik = icerik;
+            AtananKisi = atananKisi;
+            Boyut = boyut;
+        }
+
+    }
+}
